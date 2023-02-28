bed2e33d92f4ced5ac20c8fd2f76f512c790a9c4
xJANx25 <kaiyang.1125@gmail.com>
2023-02-28 09:04:01 +0800

    app.js

---
+++ app.js
@@ -0,0 +1,15 @@
+const app = Vue.createApp({
+    //data, functions
+    //template: '<h2>I am yours</h2>'
+    
+    data() {
+        return {
+            cultivationbase: '3rd martial emperor',
+            mc: 'Yang Kai',
+            cultivation: '100',
+            years: 'billion years'
+        }
+    }
+    })
+    
+    app.mount('#book')
